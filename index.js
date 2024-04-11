c9165e777827939a9e49f9b4a2424c4648433097
phandavinci <abisheksudhar@gmail.com>
2024-04-11 22:56:32 +0530

    initiate the project

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello world')
+import express from 'express';
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
